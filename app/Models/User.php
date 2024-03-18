4d2004a676157dc30eccf26ace7882f77f17f40c
Diego Chacon <diegdarch@hotmail.com>
2024-03-18 11:11:55 +0100

    Spatie: instalated, migrated tables and configurated(added use HasRoles in app\Models\User.php)

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,11 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     public $timestamps = false;//impide que se cree por defecto los campos timestamps    
 
